80c820d46cd87488a61da92497dfddbe8341d8e1
Maximiliaan Strother III <maxbax@op.pl>
2016-11-07 12:44:46 +0100

    v.0.1.0
    Working map generation and hero location

--- Adventure_Game.py
+++ Adventure_Game.py
@@ -1,1 +1,16 @@
-Some text
+import os
+os.system('clear')
+
+board_size = 25
+position_x = 12
+position_y = 10
+board = [["# "] * board_size for num in range(board_size + 1)]
+
+for x in range(1, board_size):
+    for y in range(1, board_size - 1):
+        board[x][y] = ". "
+
+board[position_x][position_y] = "@ "
+
+for line in board:
+    print("".join(line))
